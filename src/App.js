fcfe46dbae042d5847081e3a900f78a3dd2c7464
anshul malviya <malviya21anshul@gmail.com>
2022-03-14 11:27:14 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+
+const App = () => {
+
+  const [search, setSearch] = useState("Mumbai");
+  const [city, setCity] = useState(null);
+
+  useEffect(() => {
+    foreCast();
+  }, [search])
+
+  const foreCast = async () => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=511dcda4cab78272eba8ac7e872115de`
+    const response = await fetch(url);
+    const resjson = await response.json();
+    setCity(resjson.main);
+  }
+
+  const inputHandler = (e) => {
+    setSearch(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="container">
+        <div className="center_div">
+          <input type="search" placeholder="Search..." onChange={inputHandler} vlaue={search} />
+          <h1>{search}</h1>
+          {!city ?
+            (<h2>No data found</h2>) : (
+              <div>
+                <h3>Temp : {city.temp}°C</h3><br />
+                <h4>Max-temp : {city.temp_max}°C || Min-temp : {city.temp_min}°C</h4>
+              </div>)}
+        </div>
+      </div>
 
+    </>
+  )
+}
 export default App;
